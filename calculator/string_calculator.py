ee27ffe2ce9cc3bd74a277e50bfb6e555c080b1b
pooja.shinde <pooja.shinde@nihilent.com>
2025-07-28 00:47:36 +0530

    support custom single-character delimiters

--- calculator/string_calculator.py
+++ calculator/string_calculator.py
@@ -2,20 +2,20 @@ class StringCalculator:
     def __init__(self):
         self.call_count = 0
 
-    def add(self, numbers: str) -> int:
-        self.call_count += 1
-
-        if not numbers:
-            return 0
-
+    def _extract_delimiters_and_numbers(self, input_str: str):
         delimiters = [",", "\n"]
-        
-        if numbers.startswith("//"):
-            delimiter_line, numbers = numbers.split("\n", 1)
+        if input_str.startswith("//"):
+            delimiter_line, input_str = input_str.split("\n", 1)
             custom_delim = delimiter_line[2:]
             delimiters.append(custom_delim)
-        
-        
+        return delimiters, input_str
+
+    def add(self, numbers: str) -> int:
+        self.call_count += 1
+        if not numbers:
+            return 0
+        delimiters, numbers = self._extract_delimiters_and_numbers(numbers)
+
         for delimiter in delimiters:
             numbers = numbers.replace(delimiter, ",")
 
